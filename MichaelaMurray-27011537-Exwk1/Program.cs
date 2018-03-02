64219aca03d94e0882fdaa34aee47d81dcfc20cb
Michaela Murray <27011537@student.toiohomai.ac.nz>
2018-03-02 16:06:59 +1300

    Add project files.

---
+++ MichaelaMurray-27011537-Exwk1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MichaelaMurray_27011537_Exwk1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
