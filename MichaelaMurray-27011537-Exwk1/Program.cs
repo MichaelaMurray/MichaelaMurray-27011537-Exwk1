eed39e55c26bce074458a9fd02d59b9150255f90
27011537 <10012591@bcs.net.nz>
2018-03-02 19:39:08 +1300

    commit1

--- MichaelaMurray-27011537-Exwk1/Program.cs
+++ MichaelaMurray-27011537-Exwk1/Program.cs
@@ -10,6 +10,40 @@ namespace MichaelaMurray_27011537_Exwk1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Michaela-Emily Murray");
+            
+            int num1 = (12);
+            int num2 = (34);
+
+            Console.WriteLine();
+            Console.WriteLine(num1 + num2);
+
+            int num3 = (100);
+            int num4 = (5);
+
+            Console.WriteLine();
+            Console.WriteLine(num3 / num4);
+
+            Console.WriteLine();
+            Console.WriteLine("Input First Number:");
+            int num5 = (int.Parse(Console.ReadLine()));
+            Console.WriteLine("Input Second Number");
+            int num6 = (int.Parse(Console.ReadLine()));
+            int result = num5 * num6;
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+            Console.WriteLine("Please Input Number:");
+            int num7 = (int.Parse(Console.ReadLine()));
+            for (int i = 1; i <= 10; i++)
+            {
+                int ans = i * num7;
+                Console.WriteLine(ans);
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
